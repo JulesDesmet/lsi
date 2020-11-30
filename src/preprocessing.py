3070595431a06b44b1602d905dc4f2661e923ea1
JulesDesmet <jules.desmet@student.uantwerpen.be>
2020-11-30 19:25:35 +0100

    Implemented the lemmatization algorithm using NLTK

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 import csv
-from typing import Generator
-from sys import argv
+from os.path import dirname, join
+from typing import Generator, Iterable
+
+from nltk import data, pos_tag_sents, word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+
+# This should be the directory root (assuming this file is in `{product_root}/src/`)
+NLTK_PATH = join(dirname(dirname(__file__)), "nltk")
 
 
 def read_csv(filename: str) -> Generator[dict[str, str], None, None]:
@@ -24,8 +32,43 @@ def read_csv(filename: str) -> Generator[dict[str, str], None, None]:
         yield from (dict(zip(fields, row)) for row in reader)
 
 
+def lemmatize(text: Iterable[Iterable[str]]) -> Generator[str, None, None]:
+    """
+    Replaces each word in the text string with its lemma. This method uses the nltk
+    library, and requires some NLTK data in order to run. This data can be downloaded
+    by running `setup.py` in the `nltk` directory.
+
+    (https://www.geeksforgeeks.org/python-lemmatization-approaches-with-examples/)
+
+    :param text: The text that is to be lemmatised as a sequence of sentences, which are
+        sequences of words. These words/strings should only contain lowercase letters.
+    :return: A generator that yields the lemmas. These can be concatenated into a string
+        by using the " ".join(...) function.
+    """
+    # Set the NLTK data path
+    data.path = [NLTK_PATH]
+    tag_dict = {
+        "J": wordnet.ADJ,
+        "N": wordnet.NOUN,
+        "R": wordnet.ADV,
+        "V": wordnet.VERB,
+    }
+    lemmatizer = WordNetLemmatizer()
+    # Tags the words with their type/pos (part of speech)
+    # Then translates the tags using `tag_dict`
+    # And finally lemmatizes the words if possible
+    return (
+        lemmatizer.lemmatize(word, tag_dict[tag[0]]) if tag[0] in tag_dict else word
+        for sentence in pos_tag_sents(text)  # Outer loop
+        for word, tag in sentence  # Inner loop
+    )
+
+
 if __name__ == "__main__":
-    for index, line in enumerate(read_csv(argv[1])):
-        print(line)
-        if index == 10:
-            break
+    result = lemmatize(
+        [
+            "this is a test string the cat is sitting with the bats on the striped "
+            "mat under many badly flying geese".split(" ")
+        ]
+    )
+    print(" ".join(result))
