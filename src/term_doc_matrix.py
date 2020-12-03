dc8bc6623dece0e7849dfe43cd75376b7c75ca83
JulesDesmet <jules.desmet@student.uantwerpen.be>
2020-12-04 00:46:40 +0100

    Added documentation to the TF.IDF implementation (still untested)

--- src/term_doc_matrix.py
+++ src/term_doc_matrix.py
@@ -6,23 +6,39 @@ from typing import Iterable
 
 
 class TfIdf:
-    """"""
-
-    term_frequencies: list[dict[str, float]]
-    inverse_doc_frequencies: Counter
+    """
+    Collects the term frequency (TF) and inverse document frequency (IDF) scores of a
+    set of documents. All calls to `add_document()` should be done before calling the
+    `__call__()` function.
+    """
 
     def __init__(self):
-        """"""
-        self.term_frequencies = []
-        self.inverse_doc_frequencies = Counter()
+        """
+        Initialises the frequency collections.
+        """
+        self.term_frequencies: list[dict[str, float]] = []
+        self.inverse_doc_frequencies: Counter = Counter()
 
     @property
     def nr_documents(self) -> int:
-        """"""
+        """
+        Returns the number of documents that have been added so far. This function
+        relies on `term_frequencies` having a single element for every document.
+
+        :return: The current number of documents.
+        """
         return len(self.term_frequencies)
 
     def add_document(self, document: Iterable[str]) -> int:
-        """"""
+        """
+        Adds a document to the collection. The TF score is computed for each distinct
+        term in the document immediately. The IDF score can't be computed yet, and thus
+        only the occurring terms are added to the inverse document frequency mapping.
+
+        :param document: The contents of the document as an iterable collection of
+            words.
+        :return: An identifier for the added document.
+        """
         occurrences = Counter(document)
         most_common = occurrences.most_common(1)[0][1]
         self.term_frequencies.append(
@@ -32,7 +48,14 @@ class TfIdf:
         return len(self.term_frequencies) - 1
 
     def __call__(self, document_id: int, term: str) -> float:
-        """"""
+        """
+        Returns the TF.IDF score for the given term in the given document. This method
+        should only be called once all of the documents have been added.
+
+        :param document_id: The identifier of the document.
+        :param term: The term for which the TF.IDF score is being requested.
+        :return: The TF.IDF score.
+        """
         if term not in self.term_frequencies[document_id]:
             return 0.0
         tf = self.term_frequencies[document_id][term]
